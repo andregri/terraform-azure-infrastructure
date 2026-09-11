8f6824954d85f878078e7b322e46dd47514c86e9
Andrea <andrea.grillo96@live.com>
2026-09-11 16:27:31 +0200

    feat(tf): update tf module tag v1.1.1

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 module "simple" {
-  source = "git::https://github.com/andregri/terraform-module-azure-vm.git?ref=v1.1.0"
+  source = "git::https://github.com/andregri/terraform-module-azure-vm.git?ref=v1.1.1"
 
   prefix              = "simple"
   resource_group_name = var.resource_group_name
